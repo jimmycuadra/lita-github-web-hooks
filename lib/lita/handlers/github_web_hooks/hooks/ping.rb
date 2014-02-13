ba071dde7ada4a7a27e3bb225f2c650ed5257592
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-02-13 00:01:56 -0800

    Implement ping and push hook.

--- lib/lita/handlers/github_web_hooks/hooks/ping.rb
+++ lib/lita/handlers/github_web_hooks/hooks/ping.rb
@@ -1,6 +1,15 @@
 module Lita::Handlers
   module GitHubWebHooks
     class Ping < Hook
+      private
+
+      def message
+        @message ||= I18n.t("lita.handlers.github_web_hooks.ping", zen: zen)
+      end
+
+      def zen
+        payload["zen"]
+      end
     end
 
     GitHubWebHooks.register_hook("ping", Ping)
--- lib/lita/handlers/github_web_hooks/hooks/push.rb
+++ lib/lita/handlers/github_web_hooks/hooks/push.rb
@@ -1,6 +1,33 @@
 module Lita::Handlers
   module GitHubWebHooks
     class Push < Hook
+      private
+
+      def message
+        @message ||= I18n.t(
+          "lita.handlers.github_web_hooks.push",
+          name: name,
+          ref: ref,
+          repo: repo,
+          compare_url: compare_url
+        )
+      end
+
+      def compare_url
+        payload["compare"]
+      end
+
+      def name
+        payload["pusher"]["name"]
+      end
+
+      def ref
+        payload["ref"]
+      end
+
+      def repo
+        "#{payload["repository"]["owner"]["name"]}/#{payload["repository"]["name"]}"
+      end
     end
 
     GitHubWebHooks.register_hook("push", Push)
